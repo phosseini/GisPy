30e833563806e2b78b747a681b484017ff478a13
phosseini <pdr.hosseini@gmail.com>
2020-07-22 23:35:16 -0400

    adding compute_SMCAUSlme metric (a replacement for SMCAUSlsa)

--- gipsy/gist.py
+++ gipsy/gist.py
@@ -1,10 +1,20 @@
-from utils import find_mrc_word
+import torch
+import copy
+import itertools
+import statistics
+import torch.nn as nn
+
+from transformers import AutoModel, AutoTokenizer
 from nltk.corpus import wordnet as wn
+
+from utils import find_mrc_word
 
 
 class GIST:
     def __init__(self, doc):
         self.doc = doc
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-large-uncased')
+        self.model = AutoModel.from_pretrained('bert-large-uncased')
 
     def compute_SMCAUSwn(self):
         """
@@ -75,3 +85,85 @@ class GIST:
                 conc_score += (word_conc_score / len(records))
                 img_score += (word_img_score / len(records))
         return conc_score / len(self.doc), img_score / len(self.doc)
+
+    def compute_SMCAUSlme(self, pos_tags=['VERB']):
+        """
+        computing the similarity among tokens with certain POS tag in document
+        lme stands for the Language Model-based Embedding which is a replacement for Latent Semantic Analysis (LSA) here
+        :param pos_tags: list of part-of-speech tags for which we want to compute the cosine similarity
+        :return:
+        """
+        # initializing the data frame with doc data
+        df = self.doc
+
+        # creating list of tokens
+        doc_context = []
+        for index, row in df.iterrows():
+            doc_context.append(row['token_text'])
+
+        torch.set_grad_enabled(False)
+
+        doc_string = ' '.join(doc_context)
+        tokens = self.tokenizer.tokenize(doc_string)
+
+        # This is not sufficient for the model, as it requires integers as input,
+        # not a problem, let's convert tokens to ids.
+        tokens_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+
+        # Add the required special tokens
+        tokens_ids = self.tokenizer.build_inputs_with_special_tokens(tokens_ids)
+
+        # We need to convert to a Deep Learning framework specific format, let's use PyTorch for now.
+        tokens_pt = torch.tensor([tokens_ids])
+
+        # Now we're ready to go through BERT with out input
+        outputs, pooled = self.model(tokens_pt)
+
+        # creating a list of tokens and their embeddings
+        last_hidden_states = outputs[0]
+        token_embeddings = []
+        i = 0
+        while i < len(tokens):
+            token_embeddings.append([tokens[i], last_hidden_states[i + 1]])
+            i += 1
+
+        assert len(tokens) == len(token_embeddings)
+
+        i = 0
+        i_token = 0
+        verb_embeddings = []
+        while i < len(df):
+            if df.iloc[i]['token_pos'] in pos_tags:
+                # true, if there's no sub-token
+                if df.iloc[i]['token_text'].lower() == token_embeddings[i_token][0].lower():
+                    verb_embeddings.append(token_embeddings[i_token][1])
+                    i += 1
+                    i_token += 1
+                # it means that there are sub-tokens
+                else:
+                    # if you want to check the tokens
+                    # print(df.iloc[i]['token_text'], tokens[i_token])
+                    tensors = [token_embeddings[i_token][1]]
+                    j = copy.deepcopy(i_token) + 1
+
+                    # getting embeddings of all sub-tokens of current token and then computing their mean
+                    while j < len(tokens) and '#' in tokens[j]:
+                        tensors.append(token_embeddings[j][1])
+                        j += 1
+                    verb_embeddings.append(torch.mean(torch.stack(tensors), dim=0))
+                    i += 1
+                    i_token = copy.deepcopy(j)
+            else:
+                i += 1
+                i_token += 1
+
+        # checking if we have the embeddings of all VERBs
+        assert len(df.loc[df['token_pos'].isin(pos_tags)]), len(verb_embeddings)
+
+        # computing the cosine similarity among all VERBs in document
+        scores = []
+        cosine = nn.CosineSimilarity(dim=0)
+        for pair in itertools.combinations(verb_embeddings, r=2):
+            scores.append(cosine(pair[0], pair[1]).item())
+
+        return statistics.mean(scores)
