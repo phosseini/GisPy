666e714ee8ede743555e68fbc5fec201128bec72
phosseini <pdr.hosseini@gmail.com>
2021-08-11 16:13:24 -0400

    adding list of causal cues from CausalNet

--- gispy/utils.py
+++ gispy/utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sqlalchemy import create_engine, MetaData, Table, and_
 from sqlalchemy.orm import sessionmaker
 
@@ -51,3 +52,108 @@ def spacy2mrc_pos():
     }
 
     return tag_map
+
+
+def get_causal_cues():
+    """
+    getting a data frame of intra- and inter-sentence causal cues from CausalNet
+    Causal cues from the following paper: https://www.microsoft.com/en-us/research/wp-content/uploads/2016/07/12818-57567-1-PB.pdf
+    :return:
+    """
+    cols = ['cue_regex', 'cue', 'type', 'direction', 's1', 's2']
+    df = pd.DataFrame(columns=cols)
+    cues = [{'cue_regex': '(.+) lead to (.+)', 'cue': 'lead to', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) leads to (.+)', 'cue': 'leads to', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) led to (.+)', 'cue': 'led to', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) leading to (.+)', 'cue': 'leading to', 'type': 'intra', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) give rise to (.+)', 'cue': 'give rise to', 'type': 'intra', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) gave rise to (.+)', 'cue': 'gave rise to', 'type': 'intra', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) given rise to (.+)', 'cue': 'given rise to', 'type': 'intra', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) giving rise to (.+)', 'cue': 'giving rise to', 'type': 'intra', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) induces (.+)', 'cue': 'induces', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) induced (.+)', 'cue': 'induced', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) inducing (.+)', 'cue': 'inducing', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) induce (.+)', 'cue': 'induce', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) caused by (.+)', 'cue': 'caused by', 'type': 'intra', 'direction': 2, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) caused (.+)', 'cue': 'caused', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) causes (.+)', 'cue': 'causes', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) causing (.+)', 'cue': 'causing', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) cause (.+)', 'cue': 'cause', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) bring on (.+)', 'cue': 'bring on', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) brought on (.+)', 'cue': 'brought on', 'type': 'intra', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) bringing on (.+)', 'cue': 'bringing on', 'type': 'intra', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) brings on (.+)', 'cue': 'brings on', 'type': 'intra', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) result from (.+)', 'cue': 'result from', 'type': 'intra', 'direction': 2, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) resulting from (.+)', 'cue': 'resulting from', 'type': 'intra', 'direction': 2, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) results from (.+)', 'cue': 'results from', 'type': 'intra', 'direction': 2, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) resulted from (.+)', 'cue': 'resulted from', 'type': 'intra', 'direction': 2, 's1': 0,
+             's2': 1},
+            {'cue_regex': 'the reason for (.+) (is|are|was|were) (.+)', 'cue': 'the reason for', 'type': 'intra',
+             'direction': 2, 's1': 0, 's2': 2},
+            {'cue_regex': 'the reasons for (.+) (is|are|was|were) (.+)', 'cue': 'the reasons for', 'type': 'intra',
+             'direction': 2, 's1': 0, 's2': 2},
+            {'cue_regex': 'the reason of (.+) (is|are|was|were) (.+)', 'cue': 'the reason of', 'type': 'intra',
+             'direction': 2, 's1': 0, 's2': 2},
+            {'cue_regex': 'the reasons of (.+) (is|are|was|were) (.+)', 'cue': 'the reasons of', 'type': 'intra',
+             'direction': 2, 's1': 0, 's2': 2},
+            {'cue_regex': '(a|an|the|one) effect of (.+) (is|are|was|were) (.+)', 'cue': 'effect of', 'type': 'intra',
+             'direction': 1, 's1': 1, 's2': 3},
+            {'cue_regex': '(.+) (is|are|was|were) (a|an|the|one) reason for (.+)', 'cue': 'reason for', 'type': 'intra',
+             'direction': 1, 's1': 0, 's2': 3},
+            {'cue_regex': '(.+) (is|are|was|were) (a|an|the|one) reasons for (.+)', 'cue': 'reasons for',
+             'type': 'intra', 'direction': 1, 's1': 0, 's2': 3},
+            {'cue_regex': '(.+) (is|are|was|were) (a|an|the|one) reason of (.+)', 'cue': 'reason of', 'type': 'intra',
+             'direction': 1, 's1': 0, 's2': 3},
+            {'cue_regex': '(.+) (is|are|was|were) (a|an|the|one) reasons of (.+)', 'cue': 'reasons of', 'type': 'intra',
+             'direction': 1, 's1': 0, 's2': 3},
+            {'cue_regex': 'if (.+), then (.+)', 'cue': 'if', 'type': 'inter', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': 'if (.+), (.+)', 'cue': 'if', 'type': 'inter', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) because of (.+)', 'cue': 'because of', 'type': 'inter', 'direction': 2, 's1': 0,
+             's2': 1},
+            {'cue_regex': 'because (.+), (.+)', 'cue': 'because', 'type': 'inter', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+), because (.+)', 'cue': 'because', 'type': 'inter', 'direction': 2, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) because (.+)', 'cue': 'because', 'type': 'inter', 'direction': 2, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+), thus (.+)', 'cue': 'thus', 'type': 'inter', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+), therefore (.+)', 'cue': 'therefore', 'type': 'inter', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+), (.+) as a consequence', 'cue': 'as a consequence', 'type': 'inter', 'direction': 2,
+             's1': 0, 's2': 1},
+            {'cue_regex': 'inasmuch as (.+), (.+)', 'cue': 'inasmuch as', 'type': 'inter', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+), inasmuch as (.+)', 'cue': 'inasmuch as', 'type': 'inter', 'direction': 2, 's1': 0,
+             's2': 1},
+            {'cue_regex': 'in consequence of (.+), (.+)', 'cue': 'in consequence of', 'type': 'inter', 'direction': 1,
+             's1': 0, 's2': 1},
+            {'cue_regex': '(.+) in consequence of (.+)', 'cue': 'in consequence of', 'type': 'inter', 'direction': 2,
+             's1': 0, 's2': 1},
+            {'cue_regex': 'due to (.+), (.+)', 'cue': 'due to', 'type': 'inter', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) due to (.+)', 'cue': 'due to', 'type': 'inter', 'direction': 2, 's1': 0, 's2': 1},
+            {'cue_regex': 'owing to (.+), (.+)', 'cue': 'owing to', 'type': 'inter', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) owing to (.+)', 'cue': 'owing to', 'type': 'inter', 'direction': 2, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) as a result of (.+)', 'cue': 'as a result of', 'type': 'inter', 'direction': 2, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+) and hence (.+)', 'cue': 'and hence', 'type': 'inter', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+), hence (.+)', 'cue': 'hence', 'type': 'inter', 'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': 'as a consequence of (.+), (.+)', 'cue': 'as a consequence of', 'type': 'inter',
+             'direction': 1, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) as a consequence of (.+)', 'cue': 'as a consequence of', 'type': 'inter',
+             'direction': 2, 's1': 0, 's2': 1},
+            {'cue_regex': '(.+) and consequently (.+)', 'cue': 'and consequently', 'type': 'inter', 'direction': 1,
+             's1': 0, 's2': 1},
+            {'cue_regex': '(.+), consequently (.+)', 'cue': 'consequently', 'type': 'inter', 'direction': 1, 's1': 0,
+             's2': 1},
+            {'cue_regex': '(.+), for this reason alone, (.+)', 'cue': 'for this reason alone', 'type': 'inter',
+             'direction': 1, 's1': 0, 's2': 1}
+            ]
+    df = df.append(cues, ignore_index=True)
+    return df
