4887b12a5093555c7dccff3ec3fa9b1ae4b55093
phosseini <pdr.hosseini@gmail.com>
2021-09-21 17:08:40 -0400

    adding GIS z-score

--- gispy/gist.py
+++ gispy/gist.py
@@ -43,7 +43,7 @@ class GIST:
         # Step 1: reading the raw text files/documents
         if os.path.isdir(self.docs_path):
             # reading text files and building a data frame
-            df_docs = pd.DataFrame(columns=["d_id", "text", "gis"])
+            df_docs = pd.DataFrame(columns=["d_id", "text", "gis", "gis_zscore"])
             txt_files = [f for f in listdir(self.docs_path) if isfile(join(self.docs_path, f)) and '.txt' in f]
             for txt_file in txt_files:
                 with open('{}/{}'.format(self.docs_path, txt_file), 'r') as input_file:
@@ -58,26 +58,37 @@ class GIST:
             doc_ids = list(set(df_docs['d_id']))
             df_docs_tokens = convert_docs(df_docs)
 
-        scores = {"PCDCz": [], "PCREFz": [], "SMCAUSlme": [], "SMCAUSwn": [], "WRDCNCc": [], "WRDIMGc": [],
-                  "WRDHYPnv": []}
+        # ----------------
+        # initialize lists
+        scores = {}
+        normalized_scores = {}
+        z_scores = {}
+        list_of_indices = ["PCREFz", "PCDCz", "SMCAUSlme", "SMCAUSwn", "WRDCNCc", "WRDIMGc", "WRDHYPnv"]
+        for index in list_of_indices:
+            scores[index] = []  # raw scores
+            normalized_scores[index] = []  # to store normalized scores to 0-1 range
+            z_scores[index] = []  # to store z-scores of indices
+        # ----------------
 
         error_docs = []
         for doc_id in doc_ids:
             err_flag = False
             try:
                 df_doc = df_docs_tokens.loc[df_docs_tokens['d_id'] == doc_id]
-                PCREFz = self.compute_PCREFz(df_doc)
-                _, _, PCDCz = self.find_causal_connectives(df_doc)
-                SMCAUSlme = self.compute_SMCAUSlme(df_doc)
-                SMCAUSwn = self.compute_SMCAUSwn(df_doc)
-                WRDCNCc, WRDIMGc = self.compute_WRDCNCc_WRDIMGc(df_doc)
-                WRDHYPnv = self.compute_WRDHYPnv(df_doc)
+                PCREFz = self._compute_PCREFz(df_doc)
+                _, _, PCDCz = self._find_causal_connectives(df_doc)
+                SMCAUSlme = self._compute_SMCAUSlme(df_doc)
+                SMCAUSwn = self._compute_SMCAUSwn(df_doc)
+                WRDCNCc, WRDIMGc = self._compute_WRDCNCc_WRDIMGc(df_doc)
+                WRDHYPnv = self._compute_WRDHYPnv(df_doc)
             except Exception as e:
                 err_flag = True
                 error_docs.append(doc_id)
                 print('Error in computing the indexes of document: {}'.format(doc_id))
                 print(e)
 
+            # by this flag, we want to make sure that for a document, all the indices are computed without any error
+            # since for computing the final GIS, we need all indices
             if not err_flag:
                 scores["PCREFz"].append(PCREFz)
                 scores["PCDCz"].append(PCDCz)
@@ -87,12 +98,14 @@ class GIST:
                 scores["WRDIMGc"].append(WRDIMGc)
                 scores["WRDHYPnv"].append(WRDHYPnv)
 
+        # GIS formula
         # gis_score = PCREFz + PCDCz + (SMCAUSlsa - SMCAUSwn) - PCCNCz - zWRDIMGc - WRDHYPnv
 
-        # normalizing scores to 0-1 range
-        normalized_scores = {"PCREFz": [], "PCDCz": [], "SMCAUSlme": [], "SMCAUSwn": [], "WRDCNCc": [], "WRDIMGc": [],
-                             "WRDHYPnv": []}
         for idx_name, idx_values in scores.items():
+            # saving the z-score
+            z_scores[idx_name] = zscore(idx_values)
+
+            # computing the normalized score in [0, 1] range
             min_val = min(idx_values)
             max_val = max(idx_values)
             for val in idx_values:
@@ -107,12 +120,18 @@ class GIST:
                           normalized_scores["WRDCNCc"][i] - normalized_scores["WRDIMGc"][i] - \
                           normalized_scores["WRDHYPnv"][i]
 
+                doc_gis_zscore = z_scores["PCREFz"][i] + z_scores["PCDCz"][i] + (
+                        z_scores["SMCAUSlme"][i] - z_scores["SMCAUSwn"][i]) - \
+                                 z_scores["WRDCNCc"][i] - z_scores["WRDIMGc"][i] - \
+                                 z_scores["WRDHYPnv"][i]
+
                 # saving the GIS for current document
                 df_docs.loc[df_docs['d_id'] == doc_ids[i], 'gis'] = doc_gis
+                df_docs.loc[df_docs['d_id'] == doc_ids[i], 'gis_zscore'] = doc_gis_zscore
 
         return df_docs
 
-    def get_doc_sentences(self, df_doc):
+    def _get_doc_sentences(self, df_doc):
         sentences = []
         current_sentence = ""
         current_sentence_idx = 0
@@ -129,7 +148,7 @@ class GIST:
                     current_sentence_idx = df_doc.iloc[i + 1]['s_id']
         return sentences
 
-    def find_causal_verbs(self, df_doc):
+    def _find_causal_verbs(self, df_doc):
         causal_verbs = []
         verbs = list(df_doc.loc[df_doc['token_pos'] == 'VERB'].token_lemma)
         for verb in verbs:
@@ -141,7 +160,7 @@ class GIST:
                     break  # we break here since for now we only want to know whether this verb can be causal at all.
         return causal_verbs, len(causal_verbs) / len(verbs)
 
-    def find_causal_connectives(self, df_doc):
+    def _find_causal_connectives(self, df_doc):
         """
         finding the number of causal connectives in sentences in a document
         :return:
@@ -153,7 +172,7 @@ class GIST:
         patterns = []
         for idx, row in causal_cues.iterrows():
             patterns.append(re.compile(r'' + row['cue_regex'] + ''))
-        sentences = self.get_doc_sentences(df_doc)
+        sentences = self._get_doc_sentences(df_doc)
         for sentence in sentences:
             for pattern in patterns:
                 if bool(pattern.match(sentence)):
@@ -161,7 +180,7 @@ class GIST:
                     matched_patterns.append(pattern)
         return causal_connectives_count, matched_patterns, causal_connectives_count / len(sentences)
 
-    def compute_SMCAUSwn(self, df_doc):
+    def _compute_SMCAUSwn(self, df_doc):
         """
         computing the WordNet Verb Overlap in a document
         :return:
@@ -182,7 +201,7 @@ class GIST:
 
         return n_overlaps / len(pairs)
 
-    def compute_WRDHYPnv(self, df_doc):
+    def _compute_WRDHYPnv(self, df_doc):
         """
         computing the specificity of a word within the WordNet hierarchy
         :return:
@@ -210,7 +229,7 @@ class GIST:
                 scores.append(0)
         return sum(scores) / len(scores)
 
-    def compute_WRDCNCc_WRDIMGc(self, df_doc):
+    def _compute_WRDCNCc_WRDIMGc(self, df_doc):
         """
         computing the document concreteness and imageability
         :return:
@@ -231,7 +250,7 @@ class GIST:
                 img_score += (word_img_score / len(records))
         return conc_score / len(df_doc), img_score / len(df_doc)
 
-    def compute_SMCAUSlme(self, df_doc, pos_tags=['VERB']):
+    def _compute_SMCAUSlme(self, df_doc, pos_tags=['VERB']):
         """
         computing the similarity among tokens with certain POS tag in document
         lme stands for the Language Model-based Embedding which is a replacement for Latent Semantic Analysis (LSA) here
@@ -288,14 +307,14 @@ class GIST:
 
         return statistics.mean(scores)
 
-    def compute_PCREFz(self, df_doc):
+    def _compute_PCREFz(self, df_doc):
         """
         Computing Text Easability PC Referential cohesion
         :param df_doc: data frame of a document
         :return:
         """
 
-        sentences = self.get_doc_sentences(df_doc)
+        sentences = self._get_doc_sentences(df_doc)
         sentence_embeddings = self.sentence_model.encode(sentences)
 
         scores = []
