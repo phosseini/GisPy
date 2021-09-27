b4f298864f795aa84dfceef684336704a65f92cc
phosseini <pdr.hosseini@gmail.com>
2021-09-27 18:13:39 -0400

    modifying run.py

--- gispy/run.py
+++ gispy/run.py
@@ -1,14 +1,3 @@
 from gist import GIST
-from data_reader import convert_docs
-from data_reader import DataReader
 
-# loading documents
-dr = DataReader()
-doc = dr.load_input_files(count=1)
-
-# converting document to data frame
-doc_df = convert_docs(doc)
-
-# computing gist
-gist = GIST(doc=doc_df)
-print(gist.compute_SMCAUSwn())
+GIST(docs_path='../data/documents').compute_scores()
