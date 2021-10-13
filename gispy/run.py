b331f1f112cab4a02c6e089d0e60e5f97c6c8df5
phosseini <pdr.hosseini@gmail.com>
2021-10-13 15:16:29 -0400

    updating run.py to accept arguments from command line

--- gispy/run.py
+++ gispy/run.py
@@ -1,3 +1,31 @@
+import sys
+import os.path
 from gist import GIST
 
-GIST(docs_path='../data/documents').compute_scores()
+
+def main(argv):
+    usage_msg = 'Usage: python run.py [OUTPUT_FILE_NAME]'
+
+    if not os.path.exists('gist_config.json'):
+        print('error: gist_config.json could not be found. please put the config file in current directory.')
+        exit(1)
+
+    if len(argv) >= 2:
+        if argv[-1].endswith('.csv'):
+            file_path = argv[-1]
+        else:
+            print('error: OUTPUT_FILE_NAME should be *.csv')
+            exit(1)
+
+        df_scores = GIST(docs_path='../data/documents').compute_scores()
+
+        # saving the result
+        df_scores.to_csv(file_path)
+        print('computing GIS is done. results are saved at /{}'.format(file_path))
+    else:
+        print(usage_msg)
+        exit(1)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
