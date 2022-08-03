b761c27679ab5c46a64d83ee68d03ebb337c53ec
phosseini <pdr.hosseini@gmail.com>
2022-08-02 21:09:31 -0700

    Moving the gis_config.json check to the beginning

--- gispy/run.py
+++ gispy/run.py
@@ -10,8 +10,18 @@ def main(argv):
     usage_msg = 'Usage: python run.py [OUTPUT_FILE_NAME]'
 
     if not os.path.exists('gispy_config.json'):
-        print('error: gist_config.json could not be found. please put the config file in current directory.')
+        print('Error: gist_config.json could not be found. please put the config file in current directory.')
         exit(1)
+
+    if not os.path.exists('gis_config.json'):
+        print('Error: gis_config.json could not be found. The config file is required for computing GIS.')
+        exit(1)
+    else:
+        with open('gis_config.json') as f_config:
+            gis_config = json.load(f_config)
+            if any(var not in list(GisPyData().get_gispy_index_columns()) for var in list(gis_config.keys())):
+                print("Some variables in the gis_config.json are not among the GisPy indices. Check variables' names")
+                exit(1)
 
     if len(argv) >= 2:
         if argv[-1].endswith('.csv'):
@@ -23,22 +33,12 @@ def main(argv):
         start = timeit.default_timer()
         df_scores = GIST(docs_path='../data/documents').compute_indices()
 
-        # computing the final GIS scores
-        if not os.path.exists('gis_config.json'):
-            print('error: gis_config.json could not be found.')
-        else:
-            with open('gis_config.json') as f_config:
-                gis_config = json.load(f_config)
-                if any(var not in list(df_scores) for var in list(gis_config.keys())):
-                    print(
-                        'Some variables in the gis_config are not among the GisPy indices. Please check the variable names in the config file.')
-                else:
-                    try:
-                        vars_dict = GisPyData().convert_config_to_vars_dict(gis_config)
-                        df_scores = GIS().score(df_scores, vars_dict, gispy=True)
-                    except Exception as e:
-                        print('error in computing the GIS.')
-                        print(e)
+        try:
+            vars_dict = GisPyData().convert_config_to_vars_dict(gis_config)
+            df_scores = GIS().score(df_scores, vars_dict, gispy=True)
+        except Exception as e:
+            print('Error in computing the GIS.')
+            print(e)
 
         stop = timeit.default_timer()
 
